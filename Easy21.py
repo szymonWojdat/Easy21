6894c3d3367ea7c016da13f99896f64e5ce41584
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-09-06 19:36:47 +0200

    Initial commit

---
+++ Easy21.py
@@ -0,0 +1,51 @@
+from random import choice
+
+CARDS = list(range(1, 11)) + list(range(1, 11)) + list(range(-10, 0))
+
+
+class Easy21:
+	_done = None
+	_dealer = None
+	_player = None
+
+	@staticmethod
+	def _draw(black_only=False):
+		global CARDS
+		number = choice(CARDS)
+		if black_only:
+			return abs(number)
+		else:
+			return number
+
+	@staticmethod
+	def _is_busted(n):
+		return not 1 <= n <= 21
+
+	@staticmethod
+	def _dealer_draws(n):
+		return 1 <= n <= 16
+
+	def step(self, action):
+		reward = 0
+		if action == 'stick':  # dealer makes their moves
+			while self._dealer_draws(self._dealer):
+				self._dealer += self._draw()
+			if self._player > self._dealer or self._is_busted(self._dealer):
+				reward = 1
+			elif self._player < self._dealer:
+				reward = -1
+			self._done = True
+		elif action == 'hit':  # player draws a card
+			self._player += self._draw()
+			if self._is_busted(self._player):
+				reward = -1
+				self._done = True
+		else:
+			print('ERROR!!!')  # that's terrible, ik, let's keep it for now
+		return {'player': self._player, 'dealer': self._dealer}, reward, self._done
+
+	def reset(self):
+		self._dealer = self._draw(black_only=True)
+		self._player = self._draw(black_only=True)
+		self._done = False
+		return {'player': self._player, 'dealer': self._dealer}
---
+++ RLCourseAssignment.py
@@ -0,0 +1,31 @@
+from Easy21 import Easy21
+from random import choice
+import numpy as np
+
+ACTIONS = ('hit', 'stick')
+
+
+def run_episode(env):
+	total_reward = 0
+	global ACTIONS
+
+	observation = env.reset()
+	for _ in range(1000):
+		action = choice(ACTIONS)  # choosing a random action
+		observation, reward, done = env.step(action)
+		total_reward += reward
+		if done:
+			break
+	else:
+		print('Something went wrong and the loop did not break')
+		print(observation, reward, done)
+	return total_reward
+
+
+if __name__ == '__main__':
+	env1 = Easy21()
+	total = 0
+	num_episodes = 10**6
+	for _ in range(num_episodes):
+		total += run_episode(env1)
+	print('\nExpected reward = {}'.format(total/num_episodes))
