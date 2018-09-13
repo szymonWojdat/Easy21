34f4b5b90b26f4bb18931518081bf4417e516765
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-09-13 20:20:02 +0200

    Implementing Sarsa

--- RLCourseAssignment.py
+++ RLCourseAssignment.py
@@ -54,40 +54,57 @@ def learn_mc_episode(env, actions, s_a_values, s_a_reps, n0):
 	return s_a_values, s_a_reps
 
 
-def learn_sarsa_episode(env, actions, s_a_values, s_a_reps, s_a_et, n0, lambda_value):
+def learn_sarsa_episode(env, actions, s_a_values, s_a_reps, n0, lambda_value, gamma):
 	"""
 	Executes one episode of Sarsa(lambda) learning
 	:param env: environment to use
 	:param actions: action space of env
 	:param s_a_values: state-action value function (lookup table)
 	:param s_a_reps: state-action counts
-	:param s_a_reps: state-action eligibility traces (lookup table)
 	:param n0: influences epsilon in epsilon-greedy policy
 	:param lambda_value: lambda parameter value for Sarsa(lambda)
+	:param gamma: discount factor
 	:return: updated state-action value function, state-action counts and eligibility traces
 	"""
 	total_reward = 0
 	state_memory = []
 	observation = env.reset()
 	player, dealer = observation
-	for _ in range(1000):
-		# pick an episilon-greedy action
-		greedy_action = s_a_values.get_greedy_action(player, dealer)
-		ns = s_a_reps.get(player, dealer, 'hit') + s_a_reps.get(player, dealer, 'stick')
-		epsilon = n0 / (n0 + ns)
-		action = eps_greedy(actions, greedy_action, epsilon)
-		s_a_reps.increment(player, dealer, action)  # update N
-		state_memory.append(((player, dealer), action))
+	s_a_et = LookupTable(actions)
 
+	# pick an episilon-greedy action
+	greedy_action = s_a_values.get_greedy_action(player, dealer)
+	ns = s_a_reps.get(player, dealer, 'hit') + s_a_reps.get(player, dealer, 'stick')
+	epsilon = n0 / (n0 + ns)
+	action = eps_greedy(actions, greedy_action, epsilon)
+	s_a_reps.increment(player, dealer, action)  # update N
+	state_memory.append(((player, dealer), action))
+
+	for _ in range(1000):
 		observation, reward, done = env.step(action)
+		player_prime, dealer_prime = observation
 		total_reward += reward
 
-		# TODO - change this, it's basically copied from MC
-		for observation, action in state_memory:
-			player, dealer = observation
-			alpha = 1 / s_a_reps.get(player, dealer, action)
-			delta_q = alpha * (total_reward - s_a_values.get(player, dealer, action))
-			s_a_values.update(player, dealer, action, delta_q)  # update Q
+		# pick an episilon-greedy action
+		greedy_action = s_a_values.get_greedy_action(player_prime, dealer_prime)
+		ns = s_a_reps.get(player_prime, dealer_prime, 'hit') + s_a_reps.get(player, dealer, 'stick')
+		epsilon = n0 / (n0 + ns)
+		action_prime = eps_greedy(actions, greedy_action, epsilon)
+		s_a_reps.increment(player_prime, dealer_prime, action)  # update N
+
+		delta = reward + gamma * s_a_values.get(player_prime, dealer_prime, action_prime) -\
+			s_a_values.get(player, dealer, action)
+
+		for _observation, _action in state_memory:
+			_player, _dealer = _observation
+			alpha = 1 / s_a_reps.get(_player, _dealer, _action)
+			q_update = alpha * delta * s_a_et.get(_player, _dealer, _action)
+			s_a_values.update(_player, _dealer, _action, q_update)  # update Q
+			et_update = gamma * lambda_value * s_a_et.get(_player, _dealer, _action)
+			s_a_et.set(_player, _dealer, _action, et_update)
+
+		player, dealer, action = player_prime, dealer_prime, action_prime
+		state_memory.append(((player, dealer), action))
 
 		if done:
 			break
