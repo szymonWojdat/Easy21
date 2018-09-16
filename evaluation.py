0f557e36ebc9a97c4bbbca321beeeb4a840a9a7a
Szymon Wojdat <szymonwojdat13@gmail.com>
2018-09-16 10:37:27 +0200

    Removing lambda parameter from check_sarsa_discounts, adding documentation

--- evaluation.py
+++ evaluation.py
@@ -8,6 +8,15 @@ import pickle
 
 
 def learn_mc_and_sarsa(num_learn_episodes, num_run_episodes, n_zero, lambda_val, save=False):
+	"""
+	Performs Monte-Carlo and Sarsa(lambda) learning and evaluation
+	:param num_learn_episodes: number of learning episodes
+	:param num_run_episodes:  number of testing episodes
+	:param n_zero: N0 constant used for epsilon in an epsilon-greedy policy
+	:param lambda_val: lambda constant for Sarsa(lambda)
+	:param save: if True, saves the results to .pkl files
+	:return: -
+	"""
 	env1 = Easy21()
 	action_space = env1.get_action_space()
 	mc_value_table = LookupTable(action_space)
@@ -45,7 +54,14 @@ def learn_mc_and_sarsa(num_learn_episodes, num_run_episodes, n_zero, lambda_val,
 		# to open, use: pickle.load(open('monte_carlo_table.pkl', 'rb'), encoding='UTF-8') or sys.stdout.encoding
 
 
-def check_sarsa_discounts(num_learn_episodes, num_run_episodes, n_zero, lambda_val):
+def check_sarsa_discounts(num_learn_episodes, num_run_episodes, n_zero):
+	"""
+	Runs a number of Sarsa learning episodes, one batch for each value of lambda, prints average return per lambda value
+	:param num_learn_episodes: number of learning episodes
+	:param num_run_episodes: number of testing episodes
+	:param n_zero: N0 constant used for epsilon in an epsilon-greedy policy
+	:return: -
+	"""
 	env1 = Easy21()
 	action_space = env1.get_action_space()
 	sarsa_value_table = LookupTable(action_space)
@@ -68,8 +84,8 @@ def main():
 	n0 = 100
 	lmbd = 0.5
 
-	# learn_mc_and_sarsa(n_learn_ep, n_run_ep, n0, lmbd)
-	check_sarsa_discounts(n_learn_ep, n_run_ep, n0, lmbd)
+	learn_mc_and_sarsa(n_learn_ep, n_run_ep, n0, lmbd)
+	check_sarsa_discounts(n_learn_ep, n_run_ep, n0)
 
 
 if __name__ == '__main__':
